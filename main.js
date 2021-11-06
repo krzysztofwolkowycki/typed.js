defb10cddc8cdd817e6ece8b350989e45fbc9599
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2021-11-06 10:50:18 +0100

    function addLetter

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const spanText = document.querySelector('.text');
 const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sint porro nam id magnam atque, earum adipisci suscipit nostrum? Possimus dolor sit eos odio! Animi repellat aperiam tempore cumque accusamus.';
 
-setInterval(addLetter, 50);
+let indexText = 0;
+
+const addLetter = () =>{
+    spanText.textContent += txt[indexText];
+    indexText++;
+    if(indexText === txt.length) clearInterval(indexText);
+}
+
+const indexTyping = setInterval(addLetter, 50);
