f4673f19ff4e14156fbea314550264c235ee537d
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2021-11-06 11:12:32 +0100

    function cursorAnimate

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-const spanText = document.querySelector('.text');
-const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sint porro nam id magnam atque, earum adipisci suscipit nostrum? Possimus dolor sit eos odio! Animi repellat aperiam tempore cumque accusamus.';
+const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sint porro nam id magnam atque, earum adipisci suscipit nostrum? Possimus dolor sit eos odio! Animi repellat aperiam tempore cumque accusamus.'
 
 let indexText = 0;
-
+const time = 40;
 const addLetter = () =>{
-    spanText.textContent += txt[indexText];
+    spnText.textContent += txt[indexText];
     indexText++;
-    if(indexText === txt.length) clearInterval(indexText);
+    if(indexText === txt.length) clearInterval(indexTyping);
+}
+const cursorAnimate = ()=>{
+    spnCursor.classList.toggle('active');
 }
 
-const indexTyping = setInterval(addLetter, 50);
+const indexTyping = setInterval(addLetter, time);
+setInterval(cursorAnimate, 400)
+
