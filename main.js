0daf9c5a98c0740aeef501b041fd6eaba0511f57
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2021-11-06 10:39:43 +0100

    const + setInterval

---
+++ main.js
@@ -0,0 +1,4 @@
+const spanText = document.querySelector('.text');
+const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sint porro nam id magnam atque, earum adipisci suscipit nostrum? Possimus dolor sit eos odio! Animi repellat aperiam tempore cumque accusamus.';
+
+setInterval(addLetter, 50);
